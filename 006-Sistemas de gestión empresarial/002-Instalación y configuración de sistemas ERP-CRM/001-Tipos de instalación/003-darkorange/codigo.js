c13e3376d9ae37e2a2fd4112aff5f555f1787f06
= <dominiquefarias21@gmail.com>
2026-09-16 16:24:11 +0200

    .

--- 006-Sistemas de gestión empresarial/002-Instalación y configuración de sistemas ERP-CRM/001-Tipos de instalación/003-darkorange/codigo.js
+++ 006-Sistemas de gestión empresarial/002-Instalación y configuración de sistemas ERP-CRM/001-Tipos de instalación/003-darkorange/codigo.js
@@ -7,3 +7,19 @@ fetch("api/superapi.php?ruta=entidades")
             menu.innerHTML += '<a href="">' + dato + '</a>'
         })
     })
+
+fetch("api/superapi.php?ruta=tabla")
+    .then(function (respuesta) { return respuesta.json() })
+    .then(function (datos) {
+        console.log(datos.clientes[0])
+        let seccion = document.querySelector("section")
+        seccion.innerHTML += "<table>";
+        // Primero pinto las cabeceras de columna
+        seccion.innerHTML += "<tr>";
+        datos.clientes[0].forEach(function (columna) {
+            seccion.innerHTML += "<th>" + columna + "</th>";
+        })
+        seccion.innerHTML += "</tr>";
+        // Primero pinto las cabeceras de columna
+        seccion.innerHTML += "</table>";
+    }) 
